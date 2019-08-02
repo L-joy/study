d1b9d581dd0e96bfe64e2364664dab7a465f0444
Lan <1196352013@qq.com>
2019-08-02 22:07:00 +0800

    commit message

---
+++ modal/pages/index/index.vue
@@ -0,0 +1,45 @@
+<template>
+	<view class="content">
+		<button @click="openPopup">打开弹出层</button>
+		<uni-popup msg="" :show="true" type="bottom" ref="popup">
+			弹出层示例
+			<button type="primary" @click="closePopup">关闭弹出层</button>
+		</uni-popup>
+	</view>
+</template>
+
+<script>
+	import uniPopup from "../../components/uni-popup/uni-popup.vue"
+	
+	export default {
+		components: {uniPopup},
+		data() {
+			return {
+				
+			}
+		},
+		onLoad() {
+
+		},
+		methods: {
+			openPopup(){
+				this.$refs.popup.open()
+			},
+			closePopup(){
+				this.$refs.popup.close()
+			}
+
+		}
+	}
+</script>
+
+<style>
+	.content {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+
+	
+</style>
