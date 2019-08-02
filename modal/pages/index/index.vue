16f7e7e48fbebabdc9873339a35f4aefa878a6c7
Lan <1196352013@qq.com>
2019-08-02 22:55:34 +0800

    添加搜索

--- modal/pages/index/index.vue
+++ modal/pages/index/index.vue
@@ -2,7 +2,7 @@
 	<view class="content">
 		<button @click="openPopup">打开弹出层</button>
 		<uni-popup msg="" :show="true" type="bottom" ref="popup">
-			弹出层示例
+			
 			<button type="primary" @click="closePopup">关闭弹出层</button>
 		</uni-popup>
 	</view>
